b8b1eff85701fa572b3c2cceeeaf3ab391b83423
Matus232528 <120120825+Matus232528@users.noreply.github.com>
2023-09-03 17:48:51 +0300

    first commit

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+let a = 45;
+let b = 8;
+let c = 5.3;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log (typeof a)
+
+let sum = a + b;
+console.log (sum);
+
+let minus = c - b;
+console.log (minus);
+
+let multiply = a * b;
+console.log (multiply);
+
+let percent = a % b;
+console.log (percent);
+
+let division = c / a;
+console.log (division);
+
+let degree = a ** b;
+console.log (degree);
